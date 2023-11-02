a1ec2b5a75a7b2a5eceb81b12e629e1013b8d71f
John Jairo Lopez <johnjairo-2013@hotmail.com>
2023-11-01 19:43:34 -0500

    update
    se creó un prototipo de galeria web con sass
    - se agrego un fondo degrade
    - se agregaron imagenes a la galeria
    - se agregaron animaciones sutiles en scss
    
    FALTA:
    
    agregar funcionalidad a las pestañas de la galeria

---
+++ galeria/1/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@24,300,0,0" />
+    <link rel="stylesheet" href="./css/main.css">
+    <title>Document</title>
+</head>
+<body>
+    <section id="galery-project">
+        <div id="galery-imgs">
+            <div class="img-galery">
+                <a href="#"><img src="./imgs/3d.jpg" alt="1"></a>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis corrupti atque, maiores vitae illo mollitia est qui libero perferendis consequuntur nisi sint voluptatem reiciendis quo aliquid repellendus? Assumenda, vitae commodi!</p>
+            </div>
+            <div class="img-galery">
+                <a href="#"><img src="./imgs/game.jpg" alt="2"></a>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis corrupti atque, maiores vitae illo mollitia est qui libero perferendis consequuntur nisi sint voluptatem reiciendis quo aliquid repellendus? Assumenda, vitae commodi!</p>
+            </div>
+            <div class="img-galery">
+                <a href="#"><img src="./imgs/craneo.png" alt="3"></a>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis corrupti atque, maiores vitae illo mollitia est qui libero perferendis consequuntur nisi sint voluptatem reiciendis quo aliquid repellendus? Assumenda, vitae commodi!</p>
+            </div>
+            <div class="img-galery">
+                <a href="#"><img src="./imgs/web.jpg" alt="4"></a>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis corrupti atque, maiores vitae illo mollitia est qui libero perferendis consequuntur nisi sint voluptatem reiciendis quo aliquid repellendus? Assumenda, vitae commodi!</p>
+            </div>
+        </div>
+        <div id="tabs-projects">
+            <a href="#" title="web"><span class="material-symbols-outlined">language</span><span class="Span-info">Web</span></a>
+            <a href="#" title="3D"><span class="material-symbols-outlined">deployed_code</span><span class="Span-info">3D</span></a>
+            <a href="#" title="Juegos"><span class="material-symbols-outlined">sports_esports</span><span class="Span-info">Games</span></a>
+        </div>
+    </section>
+</body>
+</html>
